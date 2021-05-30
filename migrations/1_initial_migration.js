540d3363685707cb7be1a8ba9983008f27d04fe7
yystju <yystju@hotmail.com>
2021-05-30 13:43:05 +0800

    ...

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,11 @@
+const supperMaket = artifacts.require("supperMaket");
+const addressController = artifacts.require("addressController");
+const NFToken = artifacts.require("NFToken");
+const NFTMdata = artifacts.require("NFTMdata");
+const NFTBox = artifacts.require("NFTBox");
+const Commodity = artifacts.require("Commodity");
+
+module.exports = async function (deployer) {
+  // await deployer.deploy(supperMaket);
+  // await deployer.deploy(addressController);
+};
