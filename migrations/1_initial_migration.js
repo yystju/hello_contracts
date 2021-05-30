57b7e4a5dcc5113743918478042994350517c4d6
yystju <yystju@hotmail.com>
2021-05-30 21:31:01 +0800

    ...

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -5,11 +5,34 @@ const NFTMdata = artifacts.require("NFTMdata");
 const NFTBox = artifacts.require("NFTBox");
 const Commodity = artifacts.require("Commodity");
 
-module.exports = async function (deployer) {
-    console.log(`supperMaket : ${supperMaket}`);
-    const controller = await deployer.deploy(addressController);
+module.exports = async function (deployer, network, accounts) {
+    console.log(`[1_initial_migration]`);
 
-    await controller.deployed();
+    const account = accounts[0];
 
-    await deployer.deploy(supperMaket);
+    console.log(`account : ${account}`);
+
+    await deployer.deploy(addressController, account);
+    const controller = await addressController.deployed();
+
+    console.log(`controller : ${controller.address}`);
+
+    await deployer.deploy(Commodity, controller.address);
+    const commodity = await Commodity.deployed();
+
+    console.log(`commodity : ${commodity.address}`);
+
+    await deployer.deploy(NFToken);
+    const nftoken = await NFToken.deployed();
+
+    console.log(`nftoken : ${nftoken.address}`);
+
+    await deployer.deploy(supperMaket, controller.address);
+    const sm = await supperMaket.deployed();
+
+    console.log(`sm : ${sm.address}`);
+
+    await controller.addContract("COM", commodity.address);
+    await controller.addContract("SNFT", nftoken.address);
+    await controller.addContract("FEETO", sm.address);
 };
